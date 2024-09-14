06596345942f6d43b9e2f7bce40d13add1a3b160
ijakenorton <jake.norton88@gmail.com>
2024-09-14 18:52:43 +1200

    Fixed ring_buffer to not overwrite unread data

--- dummy_device_module/lab_code/asgn2/ring_buffer_kernel.c
+++ dummy_device_module/lab_code/asgn2/ring_buffer_kernel.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdbool.h>
 
-#define BUF_SIZE 10
-#define print_char(val) printf(#val " = %c\n", val)
-#define print_int(val) printf(#val " = %d\n", val)
+#include "linux/printk.h"
+
+#define BUF_SIZE 1000
+#define print_char(val) pr_info(#val " = %c\n", val)
+#define print_int(val) pr_info(#val " = %d\n", val)
 
 typedef struct {
 	char *read;
@@ -25,11 +25,10 @@ DECLARE_RINGBUFFER(ring_buffer);
 // Choosing to believe that old data is more important than new data. If the count is equal to
 // buffer size, the buffer is full and we will discard the incoming data
 //
-
 void ringbuffer_write(ringbuffer_t *ring_buffer, char value)
 {
 	if (ring_buffer->count == BUF_SIZE) {
-		printf("Buffer is full, cannot write %c\n", value);
+		pr_info("Buffer is full, cannot write %c\n", value);
 		return;
 	}
 
@@ -46,7 +45,7 @@ void ringbuffer_write(ringbuffer_t *ring_buffer, char value)
 char ringbuffer_read(ringbuffer_t *ring_buffer)
 {
 	if (ring_buffer->count == 0) {
-		printf("Buffer is empty, cannot read\n");
+		pr_info("Buffer is empty, cannot read\n");
 		return '\0';
 	}
 
@@ -58,31 +57,4 @@ char ringbuffer_read(ringbuffer_t *ring_buffer)
 
 	ring_buffer->count--;
 	return value;
-}
-
-void print_buffer(ringbuffer_t *ring_buffer)
-{
-	for (int i = 0; i < BUF_SIZE; ++i) {
-		print_char(ringbuffer_read(ring_buffer));
-	}
-}
-
-int main()
-{
-	for (char i = 'a'; i <= 'z'; i++) {
-		ringbuffer_write(&ring_buffer, i);
-	}
-
-	print_int(ring_buffer.count);
-	print_buffer(&ring_buffer);
-	print_int(ring_buffer.count);
-
-	for (char i = 'z'; i >= 'a'; i--) {
-		ringbuffer_write(&ring_buffer, i);
-	}
-
-	print_buffer(&ring_buffer);
-	print_int(ring_buffer.count);
-
-	return 0;
 }
